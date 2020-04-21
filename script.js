81fb65263cc89e3eee0f093e9a7835e22229680f
TuiNascimento <62653479+TuiNascimento@users.noreply.github.com>
2020-04-21 16:55:16 -0300

    javascriptdone , everything works, but all the css must be rebuilt, it only displays properly in a determined window size

---
+++ script.js
@@ -0,0 +1,52 @@
+
+function login(){
+    valorUsuario = document.getElementById("usuario").value
+    valorSenha = document.getElementById("senha").value
+    console.log ("valorUsuario:",valorUsuario,"//valorSenha:",valorUsuario)
+        sucessoUsuario=valorUsuario=="usuarioteste"
+        sucessoSenha=valorSenha=="senhateste"
+        emptyUsuario=valorUsuario==""
+        emptySenha=valorSenha==""
+        
+            if(sucessoUsuario && sucessoSenha){
+                alert("Login efetuado com sucesso")
+                usuario.style.borderColor=""
+                senha.style.borderColor=""
+                document.getElementById("avisoUsuario").innerHTML=""
+                document.getElementById("avisoSenha").innerHTML=""
+
+            }
+            if(emptyUsuario){
+                usuario.style.borderColor="yellow"
+                alert("Campo de usuário em branco!")
+            }
+            if(emptySenha){
+                senha.style.borderColor="yellow"
+                alert("Campo de senha em branco!")
+            }
+            if(!sucessoUsuario && !emptyUsuario){
+                document.getElementById("usuario").value=("")
+                usuario.style.borderColor="red"
+                document.getElementById("avisoUsuario").innerHTML="Usuário inválido!"
+            }
+            if(!sucessoSenha && !emptySenha){
+                senha.style.borderColor="red"
+                document.getElementById("senha").value=("")
+                document.getElementById("avisoSenha").innerHTML="Senha inválida!"
+            }
+
+}
+
+function cancel(){
+    document.getElementById("usuario").value=""
+    usuario.style.borderColor="initial"
+    document.getElementById("avisoUsuario").innerHTML=""
+    document.getElementById("senha").value=""
+    senha.style.borderColor="initial"
+    document.getElementById("avisoSenha").innerHTML=""
+    
+}
+
+function naoSei(){
+    alert("Usuario:testuser \nSenha:mypassword")
+}
